51e427f713ae54482ea0d313eb95747fe2d44262
Sanorian <krajnov.sascha@yandex.ru>
2024-07-22 12:35:13 +0400

    дописал плюсы

--- create_pointer.cpp
+++ create_pointer.cpp
@@ -2,6 +2,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xcursor/Xcursor.h>
+#include <X11/cursorfont.h>
 #include <stdio.h>
 #include <string_view>
 #include <iostream>
@@ -9,61 +10,52 @@
 #include <array>
 #include <algorithm>
 #include <cmath>
-#include <X11/cursorfont.h>
 
 #define _USE_MATH_DEFINES
 
 struct CursorCoordinates{
-    Cursor cursor;
-    int x, y, centerX, centerY;
-}
+    public:
+        Cursor cursor;
+        Window window;
+        int x, y, centerX, centerY;
+};
 
-
-void moveCursorsInCircle(std::vector<Cursor>& cursorsArray, Display* display, Window window, int centerX, int centerY, int radius, int delayMillis) {
-    const int numCursors = cursorsArray.size();
-    for (int angle = 0; angle <= 360; angle += 360 / numCursors) { // Distribute angles evenly
-        std::array<int, 2> positions[numCursors]; // Store x, y positions for each cursor
-        for (int i = 0; i < numCursors; ++i) {
-            double radians = angle * M_PI / 180.0;
-            positions[i][0] = centerX + radius * cos(radians);
-            positions[i][1] = centerY + radius * sin(radians);
-            XWarpPointer(display, None, window, 0, 0, 0, 0, positions[i][0], positions[i][1]);
-        }
-        usleep(delayMillis * 1000); // Delay for smooth animation
+std::vector<CursorCoordinates> moveCursorsInCircle(std::vector<CursorCoordinates> cursorCoordinatesVector, Display *display, int radius, double angle) {
+    for (CursorCoordinates cursorCoordinates: cursorCoordinatesVector) {
+        int newX = floor(cursorCoordinates.centerX+radius*std::cos(angle));
+        int newY = floor(cursorCoordinates.centerY+radius*std::sin(angle));
+        cursorCoordinates.x = newX;
+        cursorCoordinates.y = newY;
+        XWarpPointer(display, None, cursorCoordinates.window, newX, newY, 0, 0, 100, 100);
     }
+    return cursorCoordinatesVector;
 }
 
-Cursor createCenteredCursor(int x0, int y0, int width, int height, Display *display, Window rootWindow){
+CursorCoordinates createCenteredCursor(int x0, int y0, int width, int height, Display *display, Window rootWindow){
     CursorCoordinates cursorCoordinates;
     unsigned int shape = XC_left_ptr;
     Cursor cursor = XCreateFontCursor(display, shape);
-    XDefineCursor(display, rootWindow, cursor);
-    XWarpPointer(display, None, rootWindow, x0+floor(width/2), y0+floor(height/2), 0, 0, 100, 100);
+    Window window = XCreateSimpleWindow(display, rootWindow, x0, y0, width, height, 0, 0, 0);
+    XDefineCursor(display, window, cursor);
+    XWarpPointer(display, None, window, x0+floor(width/2), y0+floor(height/2), 0, 0, 100, 100);
 
     cursorCoordinates.cursor = cursor;
+    cursorCoordinates.window = window;
     cursorCoordinates.x = x0+floor(width/2);
     cursorCoordinates.y = y0+floor(height/2);
     cursorCoordinates.centerX = x0+floor(width/2);
     cursorCoordinates.centerY = y0+floor(height/2);
 
-    cursorCoordinatesVector.push_back(cursorCoordinates);
-    return cursor;
+    return cursorCoordinates;
 }
 
-std::vector<Cursor> createAndMoveToBaseCursors(int numberOfCursors, Display &display, Window window){
-    std::vector<Cursor> cursorsArray(numberOfCursors);
-
-    int screen = DefaultScreen(&display);
-    int screenWidth = DisplayWidth(&display, screen);
-    int screenHeight = DisplayHeight(&display, screen);
-
-    int browserWindowWidth = floor(2*screenWidth/(numberOfCursors + numberOfCursors%2));
-    int browserWindowHeight = floor(screenHeight/2);
+std::vector<CursorCoordinates> createAndMoveToBaseCursors(int numberOfCursors, Display &display, Window window, int browserWindowWidth, int browserWindowHeight, int screenWidth, int screenHeight){
+    std::vector<CursorCoordinates> cursorCoordinatesVector(numberOfCursors);
 
     int x0=0, y0=0;
 
     for(int i=0; i<numberOfCursors; i++){
-        cursorsArray[i] = createCenteredCursor(x0, y0, browserWindowWidth, browserWindowHeight, &display, window);
+        cursorCoordinatesVector[i] = createCenteredCursor(x0, y0, browserWindowWidth, browserWindowHeight, &display, window);
         if (x0+browserWindowWidth>screenWidth) {
             x0=0;
             y0+=browserWindowHeight;
@@ -72,14 +64,18 @@ std::vector<Cursor> createAndMoveToBaseCursors(int numberOfCursors, Display &dis
         }
     }
 
-    return cursorsArray;
+    return cursorCoordinatesVector;
 }
 
 int main(int argc, char** argv) {
-    std::vector<CursorCoordinates> cursorCoordinatesVector;
     using namespace std::literals;
+
     int numberOfCursors = atoi(argv[1]);
     int x = atoi(argv[2]);
+
+    double period = 2*M_PI/x;
+    double angle = 0;
+
     Display *display = XOpenDisplay(NULL);
     Window rootWindow = DefaultRootWindow(display);
     if (display == NULL) {
@@ -87,20 +83,28 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    auto cursorsArray = createAndMoveToBaseCursors(numberOfCursors, *display, rootWindow);
+    int screen = DefaultScreen(&display);
+    int screenWidth = DisplayWidth(&display, screen);
+    int screenHeight = DisplayHeight(&display, screen);
+
+    int browserWindowWidth = floor(2*screenWidth/(numberOfCursors + numberOfCursors%2));
+    int browserWindowHeight = floor(screenHeight/2);
+
+    int radius = std::min(floor(browserWindowHeight/2), floor(browserWindowWidth/2));
+
+    std::vector<CursorCoordinates> cursorCoordinatesVector = createAndMoveToBaseCursors(numberOfCursors, *display, rootWindow, browserWindowWidth, browserWindowHeight, screenWidth, screenHeight);
 
     XFlush(display);
 
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")
-    ([](crow::request& req, crow::response& res){
-        int[] = getCoordinates();
-        res.write("Hello world");
+    ([&display, &cursorCoordinatesVector, &radius, &angle, &period](crow::request& req, crow::response& res){
+        cursorCoordinatesVector = moveCursorsInCircle(cursorCoordinatesVector, display, radius, angle);
+        res.write(std::to_string(cursorCoordinatesVector[0].x) + " " + std::to_string(cursorCoordinatesVector[0].y));
+        angle += period;
         return;
     });
 
     app.port(18080).multithreaded().run();
-    // запустить движение курсоров по кругу
-
 }
